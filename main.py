9737b4d87ae12050466dfc20acb5174dc23d0eed
Hatanezumi <49899349+Hatanezumi@users.noreply.github.com>
2024-03-22 23:16:44 +0800

    v1.0.1
    1.异常捕获
    2.修复游戏进程不存在时的判断

--- main.py
+++ main.py
@@ -4,11 +4,18 @@
 @Author  : Hatanezumi
 @Contact : Hatanezumi@chunshengserver.cn
 '''
+
+version = '1.0.1'
+
 import os
 import ctypes
 import psutil
 import win32api
 import win32process
+import traceback
+
+class ProcessNotFoundError(Exception):
+    pass
 
 class MemoryProcess():
     def __init__(self, name:str) -> None:
@@ -24,8 +31,8 @@ class MemoryProcess():
             else:
                 break
         self.pid = pid[0] if len(pid) > 0 else -1
-        if pid == -1:
-            raise FileNotFoundError('未找到游戏进程')
+        if self.pid == -1:
+            raise ProcessNotFoundError('未找到游戏进程')
         self.process = win32api.OpenProcess(PROCESS_ALL_ACCESS,False,self.pid)
         self.md = ctypes.windll.LoadLibrary(os.path.join('C:','Windows','System32','kernel32.dll'))
     def close(self):
@@ -88,61 +95,71 @@ class MemoryProcess():
         '''
         return hex(self.get_memory(addr) + int(vx,16))
 
-with MemoryProcess('Raising succubus.exe') as game:
-    EXPADDR = game.get_point(game.get_point(game.get_point(game.get_point(hex(int(game.get_module_handle(),16) + int('0x00509D7C',16)),'0x2C'),'0x10'),'0x468'),'0x20')
-    MONEYADDR = game.get_point(game.get_point(game.get_point(game.get_point(hex(int(game.get_module_handle(),16) + int('0x00509D7C',16)),'0x2C'),'0x10'),'0x1B0'),'0x1C0')
-    SEXADDR = game.get_point(game.get_point(game.get_point(hex(int(game.get_module_handle(),16) + int('0x00727EF0',16)),'0x30'),'0x1B0'),'0x200')
-    RENADDR = game.get_point(game.get_point(game.get_point(game.get_point(game.get_point(hex(int(game.get_module_handle(),16) + int('0x004EF02C',16)),'0x8'),'0'),'0x18'),'0xA4'),'0x5B0')
-    HUIADDR = game.get_point(game.get_point(game.get_point(game.get_point(hex(int(game.get_module_handle(),16) + int('0x00509D7C',16)),'0x2C'),'0x10'),'0x21C'),'0x50')
-    os.system('title サキュバスの育成方法 5项修改器 v1.0.0')
-    while True:
-        os.system('cls')
-        print('=' * 50 + '\nサキュバスの育成方法 5项修改器 v1.0.0\n作者:田鼠Hatanezumi\n' + '=' * 50)
-        print('-' * 50 + '\n当前数值:')
-        print('EXP:',int(game.get_memory(EXPADDR, 'double')))
-        print('钱:',int(game.get_memory(MONEYADDR, 'double')))
-        print('性技:',int(game.get_memory(SEXADDR, 'double')))
-        print('忍耐力:',int(game.get_memory(RENADDR, 'double')))
-        print('恢复力:',int(game.get_memory(HUIADDR, 'double')))
-        print('-' * 50)
-        choose = input('请选择要修改的项目:\n1.EXP\n2.钱\n3.性技\n4.忍耐力\n5.恢复力\n0.退出\n')
-        if choose == '1':
-            value = int(input('请输入要修改的数值:'))
-            if value < 0:
-                print('输入必须大于0!')
+if __name__ == '__main__':
+    try:
+        exit = False
+        with MemoryProcess('Raising succubus.exe') as game:
+            EXPADDR = game.get_point(game.get_point(game.get_point(game.get_point(hex(int(game.get_module_handle(),16) + int('0x00509D7C',16)),'0x2C'),'0x10'),'0x468'),'0x20')
+            MONEYADDR = game.get_point(game.get_point(game.get_point(game.get_point(hex(int(game.get_module_handle(),16) + int('0x00509D7C',16)),'0x2C'),'0x10'),'0x1B0'),'0x1C0')
+            SEXADDR = game.get_point(game.get_point(game.get_point(hex(int(game.get_module_handle(),16) + int('0x00727EF0',16)),'0x30'),'0x1B0'),'0x200')
+            RENADDR = game.get_point(game.get_point(game.get_point(game.get_point(game.get_point(hex(int(game.get_module_handle(),16) + int('0x004EF02C',16)),'0x8'),'0'),'0x18'),'0xA4'),'0x5B0')
+            HUIADDR = game.get_point(game.get_point(game.get_point(game.get_point(hex(int(game.get_module_handle(),16) + int('0x00509D7C',16)),'0x2C'),'0x10'),'0x21C'),'0x50')
+            os.system(f'title サキュバスの育成方法 5项修改器 v{version}')
+            while True:
+                os.system('cls')
+                print('=' * 50 + f'\nサキュバスの育成方法 5项修改器 v{version}\n作者:田鼠Hatanezumi\n' + '=' * 50)
+                print('-' * 50 + '\n当前数值:')
+                print('EXP:',int(game.get_memory(EXPADDR, 'double')))
+                print('钱:',int(game.get_memory(MONEYADDR, 'double')))
+                print('性技:',int(game.get_memory(SEXADDR, 'double')))
+                print('忍耐力:',int(game.get_memory(RENADDR, 'double')))
+                print('恢复力:',int(game.get_memory(HUIADDR, 'double')))
+                print('-' * 50)
+                choose = input('请选择要修改的项目:\n1.EXP\n2.钱\n3.性技\n4.忍耐力\n5.恢复力\n0.退出\n')
+                if choose == '1':
+                    value = int(input('请输入要修改的数值:'))
+                    if value < 0:
+                        print('输入必须大于0!')
+                        os.system('pause')
+                        continue
+                    print('修改失败' if game.write_memory(EXPADDR, value, 'double') == 0 else '修改成功')
+                elif choose == '2':
+                    value = int(input('请输入要修改的数值:'))
+                    if value < 0:
+                        print('输入必须大于0!')
+                        os.system('pause')
+                        continue
+                    print('修改失败' if game.write_memory(MONEYADDR, value, 'double') == 0 else '修改成功')
+                elif choose == '3':
+                    value = int(input('请输入要修改的数值:'))
+                    if value < 0:
+                        print('输入必须大于0!')
+                        os.system('pause')
+                        continue
+                    print('修改失败' if game.write_memory(SEXADDR, value, 'double') == 0 else '修改成功')     
+                elif choose == '4':
+                    value = int(input('请输入要修改的数值:'))
+                    if value < 0:
+                        print('输入必须大于0!')
+                        os.system('pause')
+                        continue
+                    print('修改失败' if game.write_memory(RENADDR, value, 'double') == 0 else '修改成功') 
+                elif choose == '5':
+                    value = int(input('请输入要修改的数值:'))
+                    if value < 0:
+                        print('输入必须大于0!')
+                        os.system('pause')
+                        continue
+                    print('修改失败' if game.write_memory(HUIADDR, value, 'double') == 0 else '修改成功') 
+                elif choose == '0':
+                    exit = True
+                    break
+                else:
+                    continue
                 os.system('pause')
-                continue
-            print('修改失败' if game.write_memory(EXPADDR, value, 'double') == 0 else '修改成功')
-        elif choose == '2':
-            value = int(input('请输入要修改的数值:'))
-            if value < 0:
-                print('输入必须大于0!')
-                os.system('pause')
-                continue
-            print('修改失败' if game.write_memory(MONEYADDR, value, 'double') == 0 else '修改成功')
-        elif choose == '3':
-            value = int(input('请输入要修改的数值:'))
-            if value < 0:
-                print('输入必须大于0!')
-                os.system('pause')
-                continue
-            print('修改失败' if game.write_memory(SEXADDR, value, 'double') == 0 else '修改成功')     
-        elif choose == '4':
-            value = int(input('请输入要修改的数值:'))
-            if value < 0:
-                print('输入必须大于0!')
-                os.system('pause')
-                continue
-            print('修改失败' if game.write_memory(RENADDR, value, 'double') == 0 else '修改成功') 
-        elif choose == '5':
-            value = int(input('请输入要修改的数值:'))
-            if value < 0:
-                print('输入必须大于0!')
-                os.system('pause')
-                continue
-            print('修改失败' if game.write_memory(HUIADDR, value, 'double') == 0 else '修改成功') 
-        elif choose == '0':
-            break
-        else:
-            continue
-        os.system('pause')
+    except ProcessNotFoundError as err:
+        print(err)
+    except:
+        print('发生了意料之外的错误,详细信息:\n{}'.format(traceback.format_exc()))
+    finally:
+        os.system('pause') if not exit else None
